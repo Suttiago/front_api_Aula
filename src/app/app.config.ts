1099811b1485c6961ae9a4c099d406e8c5838ea1
Suttiago <tiagobsversut@gmail.com>
2025-08-19 21:21:44 -0300

    commit

---
+++ src/app/app.config.ts
@@ -0,0 +1,28 @@
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import { routes } from './app.routes';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import Lara from '@primeuix/themes/lara';
+import { provideHttpClient } from '@angular/common/http';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideAnimationsAsync(),
+    //INICIO IMPORTAÇÕES MANUAIS
+    provideHttpClient(),// HABILITANDO CHAMADAS HTTP
+    providePrimeNG({
+      theme: {
+        preset: Lara,
+        options: {
+          darkModeSelector: 'none'
+        }
+      }
+    })
+  ]
+};
