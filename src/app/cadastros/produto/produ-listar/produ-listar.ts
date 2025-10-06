4ed77d3d781d59d9d87928b8dc51d6e3bc1aacb8
Suttiago <150452828+Suttiago@users.noreply.github.com>
2025-10-06 14:00:18 -0300

    Update produ-listar.ts

--- src/app/cadastros/produto/produ-listar/produ-listar.ts
+++ src/app/cadastros/produto/produ-listar/produ-listar.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { ProdutoService } from '../produto-service';
+import { TableModule } from 'primeng/table';
 
 @Component({
-  selector: 'app-produ-listar',
-  imports: [],
-  templateUrl: './produ-listar.html',
-  styleUrl: './produ-listar.scss'
+  selector: 'app-produto-listar',
+  standalone: true,
+  imports: [TableModule],
+  templateUrl: './produto-listar.html',
+  styleUrl: './produto-listar.scss'
 })
-export class ProduListar {
+export class ProdutoListar implements OnInit {
+
+  produtos: any[] = [];
+
+  constructor(
+    private readonly produtoService: ProdutoService,
+    private readonly detectorMudanca: ChangeDetectorRef
+  ) {}
+
+  ngOnInit(): void {
+    this.produtoService.listarProdutos().subscribe({
+      next: (listaProdutos: any[]) => {
+        this.produtos = listaProdutos;
+        console.log(this.produtos);
+        this.detectorMudanca.detectChanges();
+      },
+      error: (error) => {
+        console.log('Erro ao buscar produtos');
+        console.log(error);
+      }
+    });
+  }
 
 }
